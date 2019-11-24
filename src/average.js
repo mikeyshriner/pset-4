bf9c225a4f40d7554974e4d38e20305f93d726e0
mikeyshriner <shrinermichael@gmail.com>
2019-11-24 18:28:21 -0500

    average is done peri@

--- src/average.js
+++ src/average.js
@@ -1,5 +1,7 @@
 const readlineSync = require("readline-sync")
 
+console.log()
+
 let add = 0
 let count = 0
 
@@ -10,13 +12,12 @@ let x = 1
 
 while (x >= 0 || count <= 1) {
     x = Number(readlineSync.question("Non-negative integer: "))
-    add = add + x
-    count = count++
+    count = count + 1
   if (x > 0 && x <= Number.MAX_SAFE_INTEGER) {
     add = add + x;
   } else if (count > 1 && x < 0) {
-    let average = (add) / (count)
-    console.log("\n" + average.toLocaleString("en", {style: 'decimal', maximumFractionDigits:3, minimumFractionDigits:3}).format(answer) + ".")
+    let average = (add) / (count - 1)
+    console.log("\n" + Intl.NumberFormat("en", {style: 'decimal', maximumFractionDigits:3, minimumFractionDigits:3}).format(average) + ".\n")
   } else {
     --count;
   }
